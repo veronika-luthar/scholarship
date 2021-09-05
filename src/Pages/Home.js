5cd14ade154870af16c37d441435b2d5d0c56f5d
Veronika Luthar <justapinglet@gmail.com>
2021-09-06 10:51:56 +1200

    imported water image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import { getByDisplayValue } from "@testing-library/dom";
 import React from "react";
+import water from './water.jpg'
 
 function Home() {
     return (
         <div>
+            <img src= {water} alt="water image" class="image"/>
           {/*   <h1>Welcome to my website</h1>*/}
         </div>
     )
