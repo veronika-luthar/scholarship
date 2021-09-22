d1c36c199f63110790e48b4be61e11da4f94c167
Veronika Luthar <justapinglet@gmail.com>
2021-09-23 10:40:42 +1200

    changed content component to header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Info from './Pages/Info';
 import Alert from './Pages/Alert';
 import {Route, Link} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Header from './Components/Content';
+import Header from './Components/Header';
 
 function App() {
   return (
