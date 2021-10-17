353f0b6ac44a76f071a6485cbaa5da462be2338d
justapinglet <justapinglet@gmail.com>
2021-10-18 12:51:40 +1300

    experimenting with chart on alert page (will move)

--- src/Pages/Alert.js
+++ src/Pages/Alert.js
@@ -1,10 +1,28 @@
 import { getByDisplayValue } from "@testing-library/dom";
 import React from "react";
+import { Bar } from "react-chartjs-2";
 
 function Alert() {
+  const chartData = {
+    labels: ["a", "b", "c", "d", "h"], 
+    datasets: [{
+      label: "bob",
+      data: [5, 10, 6, 3, 12],
+      backgroundColor: 'rgba(255,255,255,1)'
+    }]
+  };
     return (
         <div>
-          {/*   <h1>Alert</h1>*/}
+          <Bar 
+          data={chartData}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Water Data"
+              },
+            }
+          }} />
         </div>
     )
 }
