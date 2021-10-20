b7a3fc4fb85865baf40545a417d5784460429c38
justapinglet <justapinglet@gmail.com>
2021-10-21 10:47:13 +1300

    styling

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -3,40 +3,60 @@ import { Bar } from "react-chartjs-2";
 
 const Graph = () => {
     const url = window.location.href.split('/').pop();
-  //  const areas = ["SA", "NA", "ME", "AF", "AS", "EU", "AU", "NZ"];
-   
-  let dataGraph = [];
+    
+    let dataGraph = [];
 
-  switch(url) {
-    case "SA":
-        dataGraph = [1, 2, 4, 8, 2]
-        break;
-    case "NA":
-        dataGraph = [[4, 9, 2, 3, 1],["s","s","s","s","s",]]
-        break;
-    case "EU":
-        dataGraph = [{name: "helo", data: 3}, 5, 9, 1, 1]
-        break;
-    case "ME":
-        dataGraph = [8, 8, 2, 3, 3]
-        break;
-    case "AU":
-        dataGraph = [1, 2, 2, 1, 0]
-        break;  
-    case "AS":
-        dataGraph = [8, 7, 7, 9, 3]
-        break; 
-    case "AF":
-        dataGraph = [0, 1, 2, 1, 7]
-        break;
-    }
+    switch(url) {
+      case "SA":
+          dataGraph = {
+            data: [1, 2, 4, 8, 2],
+            labels: ["a", "b", "c", "d", "e"]
+          }
+          break;
+      case "NA":
+          dataGraph = {
+            data: [4, 9, 2, 3, 1],
+            labels: ["a", "b", "c", "d", "e"]
+          }
+          break;
+      case "EU":
+          dataGraph = {
+            data: [3, 5, 9, 1, 1],
+            labels: ["a", "b", "c", "d", "e"]
+          }
+          break;
+      case "ME":
+          dataGraph = {
+            data: [8, 8, 2, 3, 3],
+            labels: ["a", "b", "c", "d", "e"]
+          }
+          break;
+      case "AU":
+          dataGraph = {
+            data: [1, 2, 2, 1, 0],
+            labels: ["a", "b", "c", "d", "e"]
+          }
+          break;  
+      case "AS":
+          dataGraph = {
+            data: [8, 7, 7, 9, 3],
+            labels: ["a", "b", "c", "d", "e"]
+          }
+          break; 
+      case "AF":
+          dataGraph = {
+            data: [0, 1, 2, 1, 7],
+            labels: ["a", "b", "c", "d", "e"]
+          }
+          break;
+      }
 
     let barColor = [];
-    for(let i = 0; i < dataGraph.length; i++){
-      if(dataGraph[i] > 3 && dataGraph[i] < 5 ){
+    for(let i = 0; i < dataGraph.data.length; i++){
+      if(dataGraph.data[i] > 3 && dataGraph.data[i] < 5 ){
         barColor.push('rgba(253,253,150,1)');
       }
-      else if(dataGraph[i] > 5){
+      else if(dataGraph.data[i] > 5){
         barColor.push('rgba(255,105,97,1)');
       }
       else {
@@ -45,28 +65,38 @@ const Graph = () => {
     }
 
     const chartData = {
-        labels: dataGraph[1], 
+        labels: dataGraph.labels, 
         datasets: [{
           label: "bob",
-          data: dataGraph[0],
-          backgroundColor: barColor
+          data: dataGraph.data,
+          backgroundColor: barColor,
+          // color: "rgba(255,255,255,1)"
         }]
       };
-
     return ( <div>
         <Bar 
+        //className="white-background"
         data={chartData}
         options={{
-        //  plugins: {
-          //  title: {
-           //   display: true,
-          //    text: "Water Data"
-          //  },
-         // },
+          plugins: {
+            title: {
+              display: true,
+              text: "Water Data",
+              color: "rgba(255,255,255,1)"
+            },
+          },
          scales: {
           y: {
               suggestedMin: 0,
-              suggestedMax: 10
+              suggestedMax: 10,
+              ticks: {
+                color: "rgba(255,255,255,1)"
+              }
+          },
+          x: {
+              ticks: {
+                color: "rgba(255,255,255,1)"
+              }
           }
         }
         }} />
