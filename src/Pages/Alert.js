5afdbb4bd2f1de244b5d6cb172e6a2101ff1c4ec
justapinglet <65632432+justapinglet@users.noreply.github.com>
2021-11-16 14:28:57 +1300

    final

--- src/Pages/Alert.js
+++ src/Pages/Alert.js
@@ -8,10 +8,12 @@ import {southernMountGrandEColi} from "../Data/Southern-MountGrand";
 const Alert = () => {
 let alertText="";
 
+// alert output
 function areaAlert (area) {
     let areaName="";
     let last= "";
     
+    // determines which area data comes from
     switch(area) {
         case mountGrandEColi:
             areaName = "Mount Grand";
@@ -30,11 +32,14 @@ function areaAlert (area) {
             break;
     }
 
+    // displays alerts
     for(let i = 0; i < area.length; i++){
         if(area[i].result > 0){
+            // add area name if it is not there already
             if(last != areaName) {
                 alertText+= "<h1 class='white-text'> Boil Water alert</h1><h2 class='white-text'>" + areaName + "</h2>" + " High levels of " + area[i].name + "<br>";
             }
+            // only adds the alert text
             else {
                 alertText+= "High levels of " + area[i].name + "<br>";
             }
